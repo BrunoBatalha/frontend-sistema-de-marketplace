5856d9631010f08df7453b668e9b0e53f3c6e12f
BrunoBatalha <brunobatalha37@gmail.com>
2021-11-27 17:43:01 -0400

    refactoring

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,12 +1,22 @@
-const showToast = async function (message = "", type = "error", timeout = 3000) {
+const util = {
+  showToast: async function (message = "", type = "error", timeout = 3000) {
     const className = `md-toast-${type}`;
 
     $("#toast").addClass(className);
     $("#toast").toast("show");
-    $('#toastBody').html(message);
+    $("#toastBody").html(message);
 
     setTimeout(function () {
-        $("#toast").toast("hide");
-        $("#toast").removeClass(className);
+      $("#toast").toast("hide");
+      $("#toast").removeClass(className);
     }, timeout);
-}
+  },
+  domRender: {
+    getHtmlDetail: function (titleDetail, text) {
+      return `
+                <span class="details__title-details">${titleDetail}:</span>
+                <p class="details__text-details">${text}</p>
+            `;
+    },
+  },
+};
