70ec75d02b5541ecbdb041b951133c2a03398718
BrunoBatalha <brunobatalha37@gmail.com>
2021-11-27 19:28:16 -0400

    create in utils redirectTo and ToggledisableForm

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,9 @@
-const util = {
-  showToast: async function (message = "", type = "error", timeout = 3000) {
+const UTIL = {
+  showToast: async function (
+    message = "",
+    type = ENUMERATIONS.COLORS.ERROR,
+    timeout = 3000
+  ) {
     const className = `md-toast-${type}`;
 
     $("#toast").addClass(className);
@@ -11,12 +15,28 @@ const util = {
       $("#toast").removeClass(className);
     }, timeout);
   },
+
+  redirectTo: function (page) {
+    setTimeout(function () {
+      window.location.assign(`${page}.html`);
+    }, 3000)
+  },
+
+  toggleDisableForm: function (idForm) {
+    const isDisabled = $("form :input").prop("disabled");
+    if (idForm) {
+      $(`#${idForm} :input`).prop("disabled", !isDisabled);
+      return;
+    }
+    $("form :input").prop("disabled", !isDisabled);
+  },
+
   domRender: {
     getHtmlDetail: function (titleDetail, text) {
       return `
-                <span class="details__title-details">${titleDetail}:</span>
-                <p class="details__text-details">${text}</p>
-            `;
+            <span class="details__title-details">${titleDetail}:</span>
+            <p class="details__text-details">${text}</p>
+          `;
     },
   },
 };
