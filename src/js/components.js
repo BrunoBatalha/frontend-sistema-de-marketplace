4d540e4fe17630ba033e9bf40abc869a6aa6e69e
BrunoBatalha <brunobatalha37@gmail.com>
2021-11-27 17:43:10 -0400

    adjust layout

--- src/js/components.js
+++ src/js/components.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('#nav-links-component').html(`
-        <ul class="nav container">
+        <ul class="nav container mb-3">
             <li class="nav-item">
                 <div class="dropdown">
                     <a class="nav-link active dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
@@ -47,7 +47,7 @@ $(document).ready(function () {
                 <div class="collapse navbar-collapse" id="navBarTarget">
                     <form class="d-flex ms-auto me-3">
                         <input class="form-control" type="search" placeholder="Procurar...">
-                        <button class="btn btn-outline-success" type="submit">
+                        <button class="btn btn-outline-primary" type="submit">
                             <i class="bi bi-search"></i>
                         </button>
                     </form>
@@ -61,7 +61,7 @@ $(document).ready(function () {
                             <a class="nav-link" href="index.html">Entrar</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link" href="profile-user.html">
                                 <i class="bi bi-person-fill navbar__icon"></i>
                             </a>
                         </li>
