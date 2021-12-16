2e82ab316a596abcd21f873c9035ed46b2788bef
Cezar Augusto <49259391+cezar-augusto-bs@users.noreply.github.com>
2021-12-15 22:25:14 -0400

    resolve string

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -19,7 +19,7 @@ $(document).ready(async function () {
         const shops = await listShops();
         if (shops) {
             const allItemsHtml = shops.reduce((accumulator, item, index) => {
-                return accumulator + getHtmlCarouselItem(item.banner != "" ? item.banner : "./src/images/default-shop.jpg", index == 0, item.id, href = "profile-shop", "SHOP_ID");
+                return accumulator + getHtmlCarouselItem(item.banner != null && item.banner != "" ? item.banner : "./src/images/default-shop.jpg", index == 0, item.id, href = "profile-shop", "SHOP_ID");
             }, "");
             setCarousel(allItemsHtml, $("#carouselInnerShops"));
         }
